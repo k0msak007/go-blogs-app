20390204a9cef7c1e693bafd0d4290a7148dc5f3
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-10-08 09:34:55 +0700

    Add Update & Delete controllers

--- server/controller/blog.go
+++ server/controller/blog.go
@@ -52,9 +52,85 @@ func BlogCreate(c *fiber.Ctx) error {
 }
 
 func BlogUpdate(c *fiber.Ctx) error {
-	return nil
+
+	context := fiber.Map{
+		"statusText": "Ok",
+		"msg":        "Update Blog",
+	}
+
+	//http://localhost:8000/2
+
+	id := c.Params("id")
+
+	var record model.Blog
+
+	database.DBCon.First(&record, id)
+
+	if record.ID == 0 {
+		log.Println("Record not Found.")
+
+		context["statusText"] = ""
+		context["msg"] = "Record not Found."
+		c.Status(400)
+		return c.JSON(context)
+	}
+
+	if err := c.BodyParser(&record); err != nil {
+		log.Println("Error in parsing request.")
+
+		context["msg"] = "Something went wrong."
+		c.Status(400)
+		return c.JSON(context)
+	}
+
+	result := database.DBCon.Save(record)
+
+	if result.Error != nil {
+		log.Println("Error in saving data.")
+
+		context["msg"] = "Error in saving data."
+		c.Status(400)
+		return c.JSON(context)
+	}
+
+	context["msg"] = "Record updated successfully."
+	context["data"] = record
+
+	c.Status(200)
+	return c.JSON(context)
 }
 
+// Delete a Blog
 func BlogDelete(c *fiber.Ctx) error {
-	return nil
+
+	c.Status(400)
+	context := fiber.Map{
+		"statusText": "",
+		"msg":        "",
+	}
+
+	id := c.Params("id")
+
+	var record model.Blog
+
+	database.DBCon.First(&record, id)
+
+	if record.ID == 0 {
+		log.Println("Record not Found.")
+		context["msg"] = "Record not Found."
+
+		return c.JSON(context)
+	}
+
+	result := database.DBCon.Delete(record)
+
+	if result.Error != nil {
+		context["msg"] = "Something went wrong."
+		return c.JSON(context)
+	}
+
+	context["statusText"] = "Ok"
+	context["msg"] = "Record deleted successfully."
+	c.Status(200)
+	return c.JSON(context)
 }
